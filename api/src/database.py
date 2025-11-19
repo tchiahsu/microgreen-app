9f3675c4b4885207f8e085d0050a79baa277cecd
Tony Hsu Tai <hsutai.c@gmail.com>
2025-11-19 11:38:34 -0500

    modified to account for type errors from mysql

--- api/src/database.py
+++ api/src/database.py
@@ -1,28 +1,33 @@
 import pymysql
 import os
-from dotenv import load_dotenv 
+from dotenv import load_dotenv
 
 load_dotenv()
 
+
 def connect_db():
     """
-        Function helps to establish a connection with the microgreens_db database.
-        It gathers the user information from the env file (such as password) to connect to the database.
-        Returns the connection if successful otherwise, prints an error message and returns None.
+        Function helps to establish a connection with the microgreens_db
+        database. It gathers the user information from the env file (such as
+        password) to connect to the database. Returns the connection if
+        successful otherwise, prints an error message and returns None.
     """
     try:
-        db_connection = pymysql.connect(
+        password = os.getenv("DB_PASSWORD")
+        if password is None:
+            raise RuntimeError("DB_PASSWORD not set in environment vairables")
+
+        db_connection = pymysql.connect(  # type: ignore
             host=os.getenv("DB_HOST"),
             user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASSWORD"),
-            db=os.getenv("DB_NAME"),
+            password=password,
+            database=os.getenv("DB_NAME"),
             port=int(os.getenv("DB_PORT", 3306)),
-            charset="utf8mb4", # character encoding to read multiple formats of text
-            cursorclass=pymysql.cursors.DictCursor, # return info as dicts not tuples, easier for JSON formatting
+            charset="utf8mb4",  # char encoding to read multiple text formats
+            cursorclass=pymysql.cursors.DictCursor,  # return info as dicts
         )
         print("Connection to database successful")
         return db_connection
     except pymysql.MySQLError as e:
         print("Connection to database failed: ", e)
         return None
-
