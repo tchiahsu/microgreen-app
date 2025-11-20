3302c8b8f0fd024051dddc8208f425bf9203150f
mishell-cardenas <cardenasespinosa.m@northeastern.edu>
2025-11-19 20:48:20 -0500

    for home page chagned functions to async, included try/except, included date

--- api/src/routers/home.py
+++ api/src/routers/home.py
@@ -1,67 +1,86 @@
 from fastapi import APIRouter, HTTPException
 from src.database import connect_db
+from datetime import date
 
 router = APIRouter(prefix="/home", tags=["home"])
 
 
 @router.get("/planting_summary/{delivery_date}")
-def get_planting_summary(delivery_date: str):
+async def get_planting_summary(delivery_date: date):
     '''
     Get the planting summary for a given date (defaulted to today's date)
     Example: GET /home/planting_summary/2026-01-01
     '''
-    db = connect_db()
-    if db is None:
-        raise HTTPException(status_code=500,
-                            detail="Connection to database failed.")
+    try:
+        db = connect_db()
+        if db is None:
+            raise HTTPException(status_code=500,
+                                detail="Connection to database failed.")
 
-    cursor = db.cursor()
-    cursor.callproc("get_planting_summary", (delivery_date, ))
-    result = cursor.fetchall()
+        cursor = db.cursor()
+        cursor.callproc("get_planting_summary", (delivery_date, ))
+        result = cursor.fetchall()
 
-    cursor.close()
-    db.close()
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=400, detail=str(e))
+    
+    finally:
+        cursor.close()
+        db.close()
 
     return result
 
 
 @router.get("/germination_summary/{delivery_date}")
-def get_germination_summary(delivery_date: str):
+async def get_germination_summary(delivery_date: date):
     '''
     Get the germination summary for a given date (defaulted to today's date)
     Example: GET /home/germination_summary/2026-01-01
     '''
-    db = connect_db()
-    if db is None:
-        raise HTTPException(status_code=500,
-                            detail="Connection to database failed.")
+    try:
+        db = connect_db()
+        if db is None:
+            raise HTTPException(status_code=500,
+                                detail="Connection to database failed.")
 
-    cursor = db.cursor()
-    cursor.callproc("get_germination_summary", (delivery_date, ))
-    result = cursor.fetchall()
+        cursor = db.cursor()
+        cursor.callproc("get_germination_summary", (delivery_date, ))
+        result = cursor.fetchall()
 
-    cursor.close()
-    db.close()
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=400, detail=str(e))
+    
+    finally:
+        cursor.close()
+        db.close()
 
     return result
 
 
 @router.get("/rack_switch_summary/{delivery_date}")
-def get_rack_switch_summary(delivery_date: str):
+async def get_rack_switch_summary(delivery_date: date):
     '''
     Get the rack switch summary for a given date (defaulted to today's date)
     Example: GET /home/rack_switch_summary/2026-01-01
     '''
-    db = connect_db()
-    if db is None:
-        raise HTTPException(status_code=500,
-                            detail="Connection to database failed.")
+    try:
+        db = connect_db()
+        if db is None:
+            raise HTTPException(status_code=500,
+                                detail="Connection to database failed.")
 
-    cursor = db.cursor()
-    cursor.callproc("get_switch_summary", (delivery_date, ))
-    result = cursor.fetchall()
+        cursor = db.cursor()
+        cursor.callproc("get_switch_summary", (delivery_date, ))
+        result = cursor.fetchall()
 
-    cursor.close()
-    db.close()
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=400, detail=str(e))
+    
+    finally:
+        cursor.close()
+        db.close()
 
     return result
