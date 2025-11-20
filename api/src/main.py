f940999f2d68fb18d4c13f01cc221ca803926703
Tony Hsu Tai <hsutai.c@gmail.com>
2025-11-19 19:33:05 -0500

    added endpoints for harvest functionality

--- api/src/main.py
+++ api/src/main.py
@@ -6,12 +6,16 @@ from src.database import connect_db
 from src.routers.home import router as home_router
 from src.routers.product import router as product_router
 from src.routers.order import router as order_router
+from src.routers.harvest import router as harvest_router
+from src.routers.crop import router as crop_router
 
 app = FastAPI()
 
 app.include_router(home_router)
 app.include_router(product_router)
 app.include_router(order_router)
+app.include_router(harvest_router)
+app.include_router(crop_router)
 
 
 @app.get("/")
