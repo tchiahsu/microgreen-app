5c1f7bbd3d1efc744f2f1c7d226d1d1a289f2e26
Tony Hsu Tai <hsutai.c@gmail.com>
2025-11-20 01:23:47 -0500

    add endpoints for client

--- api/src/routers/product.py
+++ api/src/routers/product.py
@@ -4,6 +4,7 @@ from src.models.packaging import UpdatePackaging
 
 router = APIRouter(prefix="/product", tags=["product"])
 
+
 @router.put("/update_packaging/{package_id}")
 def update_packaging(package_id: int, data: UpdatePackaging):
     '''
@@ -12,8 +13,9 @@ def update_packaging(package_id: int, data: UpdatePackaging):
     '''
     db = connect_db()
     if db is None:
-        raise HTTPException(status_code=500, detail="Connection to database failed.")
-    
+        raise HTTPException(status_code=500,
+                            detail="Connection to database failed.")
+
     try:
         cursor = db.cursor()
         cursor.callproc("update_packaging_table", (package_id, data.size_type))
@@ -21,13 +23,12 @@ def update_packaging(package_id: int, data: UpdatePackaging):
         result = cursor.fetchall()
         db.commit()
         cursor.close()
-    
+
     except Exception as e:
         db.rollback()
         raise HTTPException(status_code=400, detail=str(e))
-    
+
     finally:
         db.close()
 
     return result
-
