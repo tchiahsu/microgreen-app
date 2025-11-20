d39dc87b923138da742abed86da32693e027f4e4
Tony Hsu Tai <hsutai.c@gmail.com>
2025-11-20 18:39:47 -0500

    remove unnecessary endpoints

--- api/src/main.py
+++ api/src/main.py
@@ -1,8 +1,5 @@
-from typing import Union
-
 from fastapi import FastAPI
 
-from src.database import connect_db
 from src.routers.home import router as home_router
 from src.routers.product import router as product_router
 from src.routers.order import router as order_router
@@ -27,19 +24,3 @@ app.include_router(delivery_router)
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-
-# Testing if the connection to the databse is established through the
-# test-db-connection endpoint.
-@app.get("/test-db-connection")
-def test_db_connection():
-    connection = connect_db()
-    if connection:
-        connection.close()  # close the connection
-        return {"Connection was succesful."}
-    return {"Connection was unsuccesful."}
