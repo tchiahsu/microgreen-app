5db3874afd7b753281ad47c6f1ca8d51b606b5ea
Tony Hsu Tai <hsutai.c@gmail.com>
2025-11-20 11:32:04 -0500

    added endpoints to manage deliveries

--- api/src/main.py
+++ api/src/main.py
@@ -10,6 +10,7 @@ from src.routers.harvest import router as harvest_router
 from src.routers.crop import router as crop_router
 from src.routers.client import router as client_router
 from src.routers.employee import router as employee_router
+from src.routers.delivery import router as delivery_router
 
 app = FastAPI()
 
@@ -20,6 +21,7 @@ app.include_router(harvest_router)
 app.include_router(crop_router)
 app.include_router(client_router)
 app.include_router(employee_router)
+app.include_router(delivery_router)
 
 
 @app.get("/")
